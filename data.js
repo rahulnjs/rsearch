74437053984911b13fa280b18b54e33433d6f75b
rahulnjs <rahulsharmaa@hotmail.com>
2023-06-17 12:02:29 +0530

    data integration

--- data.js
+++ data.js
@@ -55,3 +55,122 @@ window.data_set_1 = {
         }
     ]
 };
+
+
+window.data_set_2 = {
+    query: 'covid and effect',
+    result: [
+        {
+            link: "https://www.poynter.org/fact-checking/2023/brain-aneurysm-common-side-effect-covid-19-mrna-vaccine-fact-check/",
+            h3: "brain aneurysms are not a ‘common’ side effect of COVID-19 mRNA vaccines",
+            desc: "Risk factors for brain aneurysms include certain inherited disorders, untreated high blood pressure, cigarette smoking and drug abuse. There is no good evidence that the mRNA COVID-19 vaccines cause brain aneurysms,\" John Swartzberg, clinical professor emeritus at the University of California, Berkeley School of Public Health. “The claims to the contrary are not based upon any sound science or reasoning."
+
+        },
+        {
+            link: "https://www.hopkinsmedicine.org/health/conditions-and-diseases/coronavirus/is-the-covid19-vaccine-safe",
+            h3: "Is the COVID-19 Vaccine Safe?",
+            desc: "Yes. The two mRNA vaccines, Pfizer and Moderna, authorized by the U.S. Food and Drug Administration (FDA) and recommended by the Centers for Disease Control and Prevention (CDC), are very safe and very good at preventing serious or fatal cases of COVID-19. The risk of serious side effects associated with these vaccines is very small."
+        },
+        {
+            link: "https://www.ncbi.nlm.nih.gov/pmc/articles/PMC9062907/",
+            h3: "Intracranial aneurysm rupture within three days after receiving mRNA anti-COVID-19 vaccination",
+            desc: "reviewing the medical records of individuals who received a first and/ or second dose of mRNA COVID-19 vaccine between March 6, 2021, and June 14, 2021, in a rural district in Japan, the occurrences of aneurysmal SAH is identified within 3 days after mRNA vaccination. We assessed incidence rates (IRs) for aneurysmal SAH within 3 days after vaccination and spontaneous SAH for March 6–June 14, 2021, and for the March 6–June 14"
+        },
+        {
+            link: "https://tidsskriftet.no/en/2021/04/kort-kasuistikk/fatal-cerebral-haemorrhage-after-covid-19-vaccine",
+            h3: "Fatal cerebral haemorrhage after COVID-19 vaccine",
+            desc: "A young woman had a headache for a few days, before developing a fatal cerebral haemorrhage ten days after vaccination with the nCoV-19 vaccine from AstraZeneca"
+        },
+        {
+            link: "https://www.ahajournals.org/doi/10.1161/CIRCULATIONAHA.121.056135",
+            h3: "Myocarditis With COVID-19 mRNA Vaccines",
+            desc: "Myocarditis has been recognized as a rare complication of coronavirus disease 2019 (COVID-19) mRNA vaccinations, especially in young adult and adolescent males. According to the US Centers for Disease Control and Prevention",
+        },
+        {
+            link: "https://www.cureus.com/articles/147265-intracranial-hemorrhage-after-pfizer-biontech-bnt162b2-mrna-covid-19-vaccination-a-case-report#!/",
+            h3: "Intracranial Hemorrhage After Pfizer-BioNTech (BNT162b2) mRNA COVID-19 Vaccination: A Case Report",
+            desc: "In Turkey, inoculation with the Pfizer-BioNTech COVID-19 vaccine (BNT162b2, also known as tozinameran) has been approved. A 56-year-old female patient with underlying essential hypertension, experienced intracranial hemorrhage after receiving her first dose of tozinameran."
+
+        },
+        {
+            link: "https://www.cdc.gov/mmwr/volumes/71/wr/mm7144a3.htm",
+            h3: "Safety Monitoring of Bivalent COVID-19 mRNA Vaccine Booster Doses Among Persons Aged ≥12 Years",
+            desc: "On August 31, 2022, the FDA authorized bivalent formulations of BNT162b2 (Pfizer-BioNTech) and mRNA-1273 (Moderna) COVID-19 vaccines; these vaccines include mRNA encoding the spike protein from the original strain of SARS-CoV-2. Local and systemic reactions and health impacts reported during the week after a bivalent booster dose vaccination were described for v-safe registrants aged ≥12 years who received a bivalent booster dose during August 31–October 23, 2022."
+        },
+        {
+            link: "https://www.ncbi.nlm.nih.gov/pmc/articles/PMC8894799/",
+            h3: " Stroke Associated with COVID-19 Vaccines",
+            desc: "Development of safe and effective vaccines against coronavirus disease 2019 (COVID-19) remains the cornerstone of controlling this pandemic. However, there are increasing reports of various types of stroke including ischemic stroke, and hemorrhagic stroke, as well as cerebral venous sinus thrombosis (CVST) after COVID-19 vaccination."
+        },
+        {
+            link: "https://www.mayoclinichealthsystem.org/hometown-health/featured-topic/covid-19-vaccine-myths-debunked",
+            h3: "COVID-19 vaccine myths debunked",
+            desc: "The FDA has approved the Pfizer-BioNTech COVID-19 vaccine, now called Comirnaty, to prevent COVID-19 in people age 16 and older. The FDA approved Comirnaty after data found the vaccine is safe and effective."
+        },
+        {
+            link: "https://www.cedars-sinai.org/newsroom/new-data-shows-covid-19-vaccine-does-not-raise-stroke-risk/",
+            h3: "New Data Show COVID-19 Vaccine Does Not Raise Stroke Risk",
+            desc: "Cedars-Sinai Experts Say, However, That While COVID-19 Vaccines Don’t Increase the Risk of Stroke, Infection Does"
+        }
+    ]
+};
+
+
+window.data_set_3 = {
+    query: 'covid and effect',
+    result: [
+        {
+            link: "https://www.ncbi.nlm.nih.gov/pmc/articles/PMC9062907/",
+            h3: "Intracranial aneurysm rupture within three days after receiving mRNA anti-COVID-19 vaccination: Three case reports",
+            desc: "A 44-year-old woman who had no medical history presented with severe headache 4 h after receiving the second dose of the BNT162b2 mRNA COVID-19 vaccine. Three-dimensional CT angiography showed a saccular aneurysm arising from a bifurcation in the left MCA "
+
+        },
+        {
+            link: "https://tidsskriftet.no/en/2021/04/kort-kasuistikk/fatal-cerebral-haemorrhage-after-covid-19-vaccine",
+            h3: "Fatal cerebral haemorrhage after COVID-19 vaccine",
+            desc: " healthy woman in her thirties with headaches that developed one week after vaccination with ChAdOx1 nCoV-19. Three days later, her condition deteriorated rapidly, and she presented to the emergency department with slurred speech, uncoordinated movements and reduced consciousness. Symptoms progressed to left-sided hemiparesis and her level of consciousness deteriorated. the patient died the following day. "
+        },
+        {
+            link: "https://www.poynter.org/fact-checking/2023/brain-aneurysm-common-side-effect-covid-19-mrna-vaccine-fact-check/",
+            h3: "brain aneurysms are not a ‘common’ side effect of COVID-19 mRNA vaccines",
+            desc: "Risk factors for brain aneurysms include certain inherited disorders, untreated high blood pressure, cigarette smoking and drug abuse. There is no good evidence that the mRNA COVID-19 vaccines cause brain aneurysms,\" John Swartzberg, clinical professor emeritus at the University of California, Berkeley School of Public Health. “The claims to the contrary are not based upon any sound science or reasoning."
+
+        },
+        {
+            link: "https://www.hopkinsmedicine.org/health/conditions-and-diseases/coronavirus/is-the-covid19-vaccine-safe",
+            h3: "Is the COVID-19 Vaccine Safe?",
+            desc: "Yes. The two mRNA vaccines, Pfizer and Moderna, authorized by the U.S. Food and Drug Administration (FDA) and recommended by the Centers for Disease Control and Prevention (CDC), are very safe and very good at preventing serious or fatal cases of COVID-19. The risk of serious side effects associated with these vaccines is very small."
+        },
+        {
+            link: "https://www.mayoclinichealthsystem.org/hometown-health/featured-topic/covid-19-vaccine-myths-debunked",
+            h3: "Debunking COVID-19 myths",
+            desc: "Getting a COVID-19 vaccine reduces the risk of infection with the COVID-19 virus and slows the spread of COVID-19. Also, vaccination can prevent severe illness from COVID-19. But misinformation continues to circulate about COVID-19 vaccines and ways to prevent infection with the COVID-19 virus",
+        },
+        {
+            link: "https://www.cedars-sinai.org/newsroom/new-data-shows-covid-19-vaccine-does-not-raise-stroke-risk/",
+            h3: "New Data Show COVID-19 Vaccine Does Not Raise Stroke Risk",
+            desc: "Newly compiled data evaluated by researchers in the Department of Neurology and the Smidt Heart Institute at Cedars-Sinai shows that COVID-19 vaccines do not raise stroke risk. Stroke risk is great for those who have lung or cardiovascular disease, high blood pressure, high cholesterol, diabetes, a prior stroke, and people who are immunocompromised, Simpkins said, adding that patients 65 and older have been reported to face increased stroke risk."
+
+        },
+        {
+            link: "https://www.cdc.gov/coronavirus/2019-ncov/vaccines/safety/safety-of-vaccines.html",
+            h3: "Safety of COVID-19 Vaccines",
+            desc: "COVID-19 vaccines are safe and effective. COVID-19 vaccines were evaluated in tens of thousands of participants in clinical trials. The vaccines met the Food and Drug Administration’s (FDA’s) rigorous scientific standards for safety, effectiveness, and manufacturing quality needed to support emergency use authorization"
+        },
+        {
+            link: "https://www.southnassau.org/sn/vaccine-information",
+            h3: "COVID-19 Vaccination Information and Resources",
+            desc: "The Food and Drug Administration has granted full approval to the Pfizer-BioNTech COVID-19 vaccine for people ages 12 and older, and to the Moderna vaccine for those 18 and older. Serious side effects are extremely rare. Nearly 8 billion doses of the COVID-19 vaccines have been given worldwide—in fact, the COVID-19 vaccines are the most closely monitored vaccines in history."
+        },
+        {
+            link: "https://www.cureus.com/articles/147265-intracranial-hemorrhage-after-pfizer-biontech-bnt162b2-mrna-covid-19-vaccination-a-case-report#!/",
+            h3: "Intracranial Hemorrhage After Pfizer-BioNTech (BNT162b2) mRNA COVID-19 Vaccination: A Case Report",
+            desc: "a 56-year-old female patient with underlying essential hypertension who experienced intracranial hemorrhage after receiving her first dose of tozinameran. The patient underwent immediate surgical evacuation of the hematoma. She was pronounced deceased on the second postoperative day."
+        },
+        {
+            link: "https://www.cdc.gov/mmwr/volumes/71/wr/mm7144a3.htm",
+            h3: "Safety Monitoring of Bivalent COVID-19 mRNA Vaccine Booster Doses Among Persons Aged ≥12 Years",
+            desc: "Preliminary safety findings after bivalent booster vaccination among persons aged ≥12 years are similar to those after monovalent booster vaccination (3–5). Health care providers and patients can be reassured that adverse events reported after a bivalent booster dose are consistent with those reported after monovalent doses. Health impacts after COVID-19 vaccination are less frequent and less severe than those associated with COVID-19 illness."
+        }
+    ]
+};
